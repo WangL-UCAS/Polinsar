1e71f70131bdd213c79bb674948e36f00a1e1a1a
Wangliang <1461886766@qq.com>
2025-03-20 23:21:26 +0800

    计算配准后数据的T6矩阵

--- make_T6.py
+++ make_T6.py
@@ -4,7 +4,7 @@ import math
 
 
 """
-    data_ : 是指的卫星数据的日期，例如20240829； 
+    data_ : 是指的卫星数据的日期，例如20240829；
     number_id 指的是卫星标号：例如105058528
     envi配准后的结果标识：
     saocom_20240829_105058528_QS6_D_HH_slc_rsp
@@ -59,13 +59,52 @@ print("测试一下数据读取是否成功读取：", array_HH_909[5000][2000],
 def make_pauli(array_HH, array_VV, array_HV):
     k1 = (array_HH + array_VV) / np.sqrt(2)
     k2 = (array_HH - array_VV) / np.sqrt(2)
-    k3 = 2 * array_HV
+    k3 = 2 * array_HV / np.sqrt(2)
     return k1, k2, k3
 
 # 计算 Pauli 分解
-array_HH_830_k1, array_HH_830_k2, array_HH_830_k3 = make_pauli(array_HH_830, array_VV_830, array_HV_830)
-array_HH_909_k1, array_HH_909_k2, array_HH_909_k3 = make_pauli(array_HH_909, array_VV_909, array_HV_909)
+array_830_k1, array_830_k2, array_830_k3 = make_pauli(array_HH_830, array_VV_830, array_HV_830)
+array_909_k1, array_909_k2, array_909_k3 = make_pauli(array_HH_909, array_VV_909, array_HV_909)
 
 print("-------pauli 计算结束-------")
 
+array_830_k1_H = array_830_k1.T.conj()
+array_830_k2_H = array_830_k2.T.conj()
+array_830_k3_H = array_830_k3.T.conj()
+k = np.array([[array_830_k1, array_830_k2, array_830_k3]])
+# k1 = np.array([[array_830_k1_H],[array_830_k2_H],[array_830_k3_H]])
+print(k.shape)
+"""
+    ------- 计算 T6 并保存---------
+    transpose_conjugate = matrix.T.conj()  矩阵转置共轭函数 matrix 是原始函数
+     k1 = np.array([array_mask_k1, array_mask_k2, array_mask_k3]).T
 
+"""
+def make_T6(array_mask_k1, array_mask_k2, array_mask_k3, array_slave_k1, array_slave_k2, array_slave_k3):
+
+    """
+    计算矩阵转置共轭
+    参数说明： K1是k6矩阵中的前三项、k2为后三项 ;K6 = [array_830_k1, array_830_k2, array_830_k3, array_909_k1, array_909_k2, array_909_k3]
+    k1_T 表示是k1 的转置，也就是从三行一列转为一行三列用于后续的计算
+    而其中元素（array_830_k1、、、、）的的转置共轭在外面已经计算  例如： array_mask_k1_T = array_mask_k1.T.conj()
+    Omaga_11 表示T6矩阵中的极化干涉矩阵，如果将T6的 6x6矩阵分为四块；那么Omaga_11 表示的是右上角的 3x3矩阵
+
+    """
+
+    array_mask_k1_T = array_mask_k1.T.conj()
+    array_mask_k2_T = array_mask_k2.T.conj()
+    array_mask_k3_T = array_mask_k3.T.conj()
+    k1 = np.array([[array_mask_k1],[array_mask_k2],[array_mask_k3]]) # 这里的数组大小为 (3,1,row,col)
+    k1_T = np.array([[array_mask_k1_T,array_mask_k2_T,array_mask_k3_T]]) # 这里由于上面对元素进行了转置并在外面手动加入转置因此这里的数组大小为(1,3,col,row)
+    T11 = k1 * k1_T
+
+    array_slave_k1_T = array_slave_k1.T.conj()
+    array_slave_k2_T = array_slave_k2.T.conj()
+    array_slave_k3_T = array_slave_k3.T.conj()
+
+    k2 = np.array([[array_slave_k1],[array_slave_k2],[array_slave_k3]])
+    k2_T = np.array([[array_slave_k1_T, array_slave_k2_T, array_slave_k3_T]])
+    T22 = k2 * k2_T
+
+    Omaga_11 = k2 * k1_T
+    return T11, T22, Omaga_11
