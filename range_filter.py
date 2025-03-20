2227f72761c768da8cf50030075397d159802268
Wangliang <1461886766@qq.com>
2025-03-20 23:21:35 +0800

    实现频谱滤波

--- range_filter.py
+++ range_filter.py
@@ -3,13 +3,17 @@ import numpy as np
 from scipy.fft import fft2, ifft2, fftshift, ifftshift
 from osgeo import gdal
 import matplotlib.pyplot as plt
+from datetime import datetime
 
+
+strat_time = datetime.now()
+print("当前时间为：",datetime.now())
 # 物理参数
 c = 299792458  # 光速 (m/s)
 lambda_radar = 0.235  # 雷达波长 (m)
 baseline = 107.514  # 垂直基线 (m)
-x_spacing = 3.87737348876680254505799894105
- # 斜距方向像素间距 (m)
+x_spacing_830 = 11.242217175 #isce运行的时候可以看的到
+x_spacing_909 = 11.242217175 # 斜距方向像素间距 (m)
 system_bandwidth = 23.68e6  # SAR 系统带宽 (Hz)
 
 # 读取SAR数据
@@ -18,8 +22,8 @@ def read_gdal_array(filepath):
     return dataset.GetRasterBand(1).ReadAsArray()
 
 #读取配准数据
-array_830 = read_gdal_array("830/saocom_20240829_105058528_QS6_D_VV_slc_rsp")
-array_909 = read_gdal_array("909/saocom_20240906_105435075_QS6_D_VV_slc_rsp")
+array_830 = read_gdal_array("830/saocom_20240829_105058528_QS6_D_HV_slc_rsp")
+array_909 = read_gdal_array("909/saocom_20240906_105435075_QS6_D_HV_slc_rsp")
 #读取视角信息
 angle_830 = np.radians(read_gdal_array("830/look_angle_resize.dat"))
 angle_909 = np.radians(read_gdal_array("909/look_angle_resize.dat"))
@@ -35,8 +39,8 @@ f_909 = np.zeros((rows_909, cols_909))
 print(array_830.shape, array_909.shape)
 
 
-range_near_830 = 696371.072  # 最近距离
-range_near_909 = 696385.990
+range_near_830 = 696371.072  # 最近距离  isce运行日志里面有
+range_near_909 = 696389.97726431070
 #计算
 def f_fix(shape, angle,slope, range_near,x):
     f_ = np.zeros(shape)
@@ -51,8 +55,8 @@ def f_fix(shape, angle,slope, range_near,x):
                 f_[i, j] = 0  # 或者其他合适的处理方式
     return f_
 
-f_830 = f_fix(array_830.shape, angle_830,slope_angle_830,range_near_830,x_spacing)
-f_909 = f_fix(array_909.shape, angle_909,slope_angle_909,range_near_909,x_spacing)
+f_830 = f_fix(array_830.shape, angle_830,slope_angle_830,range_near_830,x_spacing_830)
+f_909 = f_fix(array_909.shape, angle_909,slope_angle_909,range_near_909,x_spacing_909)
 
 # 计算 FFT
 fft_830 = fftshift(fft2(array_830))
@@ -116,33 +120,36 @@ def save_complex_as_complex(output_path, complex_array, reference_file):
     print(f"复数数据已保存为复数格式文件: {output_path}")
 
 # 保存 830 和 909 结果
-save_complex_as_complex("filtered_image_830_complex.dat", filtered_image_830, "830/saocom_20240829_105058528_QS6_D_VV_slc_rsp")
-save_complex_as_complex("filtered_image_909_complex.dat", filtered_image_909, "909/saocom_20240906_105435075_QS6_D_VV_slc_rsp")
+save_complex_as_complex("filtered_image_830_HV.dat", filtered_image_830, "830/saocom_20240829_105058528_QS6_D_HH_slc_rsp")
+save_complex_as_complex("filtered_image_909_HV.dat", filtered_image_909, "909/saocom_20240906_105435075_QS6_D_HH_slc_rsp")
 
-plt.imshow(filter_830, cmap='gray')
-plt.title("Filter 830")
-plt.colorbar()
-plt.show()
-
-print("结束运行")
-# 结果可视化（分别显示实部和虚部）
-plt.figure(figsize=(12, 6))
-
-plt.subplot(2, 2, 1)
-plt.title("Filtered Image 830 - Real Part")
-plt.imshow(np.real(filtered_image_830), cmap='gray')
-
-plt.subplot(2, 2, 2)
-plt.title("Filtered Image 830 - Imaginary Part")
-plt.imshow(np.imag(filtered_image_830), cmap='gray')
-
-plt.subplot(2, 2, 3)
-plt.title("Filtered Image 909 - Real Part")
-plt.imshow(np.real(filtered_image_909), cmap='gray')
-
-plt.subplot(2, 2, 4)
-plt.title("Filtered Image 909 - Imaginary Part")
-plt.imshow(np.imag(filtered_image_909), cmap='gray')
-
-plt.show()
+end_time = datetime.now()
+print("运行结束，时间为", datetime.now())
+print("程序共计运行时间为：",(end_time - strat_time))
+# plt.imshow(filter_830, cmap='gray')
+# plt.title("Filter 830")
+# plt.colorbar()
+# plt.show()
+#
+# print("结束运行")
+# # 结果可视化（分别显示实部和虚部）
+# plt.figure(figsize=(12, 6))
+#
+# plt.subplot(2, 2, 1)
+# plt.title("Filtered Image 830 - Real Part")
+# plt.imshow(np.real(filtered_image_830), cmap='gray')
+#
+# plt.subplot(2, 2, 2)
+# plt.title("Filtered Image 830 - Imaginary Part")
+# plt.imshow(np.imag(filtered_image_830), cmap='gray')
+#
+# plt.subplot(2, 2, 3)
+# plt.title("Filtered Image 909 - Real Part")
+# plt.imshow(np.real(filtered_image_909), cmap='gray')
+#
+# plt.subplot(2, 2, 4)
+# plt.title("Filtered Image 909 - Imaginary Part")
+# plt.imshow(np.imag(filtered_image_909), cmap='gray')
+#
+# plt.show()
 
