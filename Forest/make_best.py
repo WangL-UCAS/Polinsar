33c4f7871d842772690c0a947d5f7287612df977
Wangliang <1461886766@qq.com>
2025-03-31 11:22:21 +0800

    森林高度反演计算

--- Forest/make_best.py
+++ Forest/make_best.py
@@ -60,9 +60,9 @@ def make_T6_and_MAX(T11,T22,Omaga12, row, col):
     eig_mid = np.take_along_axis(eig_values1, sorted_indices[..., 1:2], axis=-1).squeeze()  # (row, col)
     eig_end = np.take_along_axis(eig_values1, sorted_indices[..., 2:3], axis=-1).squeeze()  # (row, col)
 
-    Angle_max = np.angle((vec1_max.conj() * vec2_max).sum(axis=-1))
-    Angle_mid = np.angle((vec1_mid.conj() * vec2_mid).sum(axis=-1))
-    Angle_end = np.angle((vec1_end.conj() * vec2_end).sum(axis=-1))
+    Angle_max = np.angle((vec1_max.conj() * vec2_max_).sum(axis=-1))
+    Angle_mid = np.angle((vec1_mid.conj() * vec2_mid_).sum(axis=-1))
+    Angle_end = np.angle((vec1_end.conj() * vec2_end_).sum(axis=-1))
 
     Y_MAX = np.sqrt(eig_max) * np.exp(-1j * Angle_max)  # (row, col)
     Y_MID = np.sqrt(eig_mid) * np.exp(-1j * Angle_mid)  # (row, col)
@@ -70,3 +70,171 @@ def make_T6_and_MAX(T11,T22,Omaga12, row, col):
 
     print("--------- T11, T22, Ω12 计算完成，并已计算最优复相干系数 -----------")
     return Y_MAX, Y_MID, Y_END
+
+
+def pdopt(tm, om, numph=30, step=50, reg=0.0, returnall=False):
+    dim = np.shape(tm)
+
+    # Matrix regularization:
+    if reg > 0:
+        regmat = np.zeros(dim, dtype='complex64')
+        regmat[:, :] = np.eye(dim[2])
+        regmat = regmat * reg * np.trace(tm, axis1=2, axis2=3)[:, :, np.newaxis, np.newaxis]
+        tm = tm + regmat
+
+        regmat = np.zeros(dim, dtype='complex64')
+        regmat[:, :] = np.eye(dim[2])
+        regmat = regmat * reg * np.trace(om, axis1=2, axis2=3)[:, :, np.newaxis, np.newaxis]
+        om = om + regmat
+        del regmat
+
+    # Arrays to store coherence separation, and the two complex coherence values.
+    cohsize = (dim[0], dim[1])  # number of az, rng pixels
+    cohdiff = np.zeros(cohsize, dtype='float32')
+    gammamax = np.zeros(cohsize, dtype='complex64')
+    gammamin = np.zeros(cohsize, dtype='complex64')
+
+    # Arrays to store minor axis coherences.
+    mincohdiff = np.ones(cohsize, dtype='float32') * 99
+    gammaminormax = np.zeros(cohsize, dtype='complex64')
+    gammaminormin = np.zeros(cohsize, dtype='complex64')
+
+    # Arrays to store polarimetric weighting vectors for the optimized coherences.
+    weightsize = (dim[0], dim[1], dim[3])
+    wmax = np.zeros(weightsize, dtype='complex64')
+    wmin = np.zeros(weightsize, dtype='complex64')
+
+    # Main Loop
+    for Ph in np.arange(0, numph):  # loop through rotation angles
+        Pr = Ph * np.pi / numph  # phase shift to be applied
+
+        print('kapok.cohopt.pdopt | Current Progress: ' + str(
+            np.round(Pr / np.pi * 100, decimals=2)) + '%. (' + time.ctime() + ')     ', end='\r')
+
+        for az in range(0, dim[0], step):
+            azend = az + step
+            if azend > dim[0]:
+                azend = dim[0]
+
+            for rng in range(0, dim[1], step):
+                rngend = rng + step
+                if rngend > dim[1]:
+                    rngend = dim[1]
+
+                omblock = om[az:azend, rng:rngend]
+                tmblock = tm[az:azend, rng:rngend]
+                z12 = omblock.copy()
+
+                # Apply phase shift to omega matrix:
+                z12 = z12 * np.exp(1j * Pr)
+                z12 = 0.5 * (z12 + np.rollaxis(np.conj(z12), 3, start=2))
+
+                # Check if any pixels have singular covariance matrices.
+                # If so, set those matrices to the identity, to keep an
+                # exception from being thrown by linalg.inv().
+                det = linalg.det(tmblock)
+                ind = (det == 0)
+                if np.any(ind):
+                    tmblock[ind] = np.eye(dim[3])
+
+                # Solve the eigenvalue problem:
+                nu, w = linalg.eig(np.einsum('...ij,...jk->...ik', linalg.inv(tmblock), z12))
+
+                wH = np.rollaxis(np.conj(w), 3, start=2)
+
+                Tmp = np.einsum('...ij,...jk->...ik', omblock, w)
+                Tmp12 = np.einsum('...ij,...jk->...ik', wH, Tmp)
+
+                Tmp = np.einsum('...ij,...jk->...ik', tmblock, w)
+                Tmp11 = np.einsum('...ij,...jk->...ik', wH, Tmp)
+
+                azind = np.tile(np.arange(0, w.shape[0]), (w.shape[1], 1)).T
+                rngind = np.tile(np.arange(0, w.shape[1]), (w.shape[0], 1))
+
+                lmin = np.argmin(nu, axis=2)
+                gmin = Tmp12[azind, rngind, lmin, lmin] / np.abs(Tmp11[azind, rngind, lmin, lmin])
+
+                lmax = np.argmax(nu, axis=2)
+                gmax = Tmp12[azind, rngind, lmax, lmax] / np.abs(Tmp11[azind, rngind, lmax, lmax])
+
+                ind = (np.abs(gmax - gmin) > cohdiff[az:azend, rng:rngend])
+
+                # If we've found the coherences with the best separation
+                # so far, save them.
+                if np.any(ind):
+                    (azupdate, rngupdate) = np.where(ind)
+
+                    cohdiff[az + azupdate, rng + rngupdate] = np.abs(gmax - gmin)[azupdate, rngupdate]
+                    gammamax[az + azupdate, rng + rngupdate] = gmax[azupdate, rngupdate]
+                    gammamin[az + azupdate, rng + rngupdate] = gmin[azupdate, rngupdate]
+
+                    if returnall:
+                        wmax[az + azupdate, rng + rngupdate, :] = np.squeeze(
+                            w[azupdate, rngupdate, :, lmax[azupdate, rngupdate]])
+                        wmin[az + azupdate, rng + rngupdate, :] = np.squeeze(
+                            w[azupdate, rngupdate, :, lmin[azupdate, rngupdate]])
+
+                # If returnall is True, also check if this coherence pair
+                # has the smallest separation found so far.
+                if returnall:
+                    ind = (np.abs(gmax - gmin) < mincohdiff[az:azend, rng:rngend])
+
+                    if np.any(ind):
+                        (azupdate, rngupdate) = np.where(ind)
+
+                        mincohdiff[az + azupdate, rng + rngupdate] = np.abs(gmax - gmin)[azupdate, rngupdate]
+                        gammaminormax[az + azupdate, rng + rngupdate] = gmax[azupdate, rngupdate]
+                        gammaminormin[az + azupdate, rng + rngupdate] = gmin[azupdate, rngupdate]
+
+    print('kapok.cohopt.pdopt | Optimization complete. (' + time.ctime() + ')          ')
+    if returnall:
+        return gammamax, gammamin, gammaminormax, gammaminormin, wmax, wmin
+    else:
+        return gammamax, gammamin
+
+
+def pdopt_pixel(tm, om, numph=60, reg=0.0):
+    cohdiff = 0
+    gammaregion = np.empty((numph * 2 + 1), dtype='complex')
+
+    # Matrix regularization:
+    if reg > 0:
+        tm = tm + reg * np.trace(tm) * np.eye(3)
+        om = om + reg * np.trace(om) * np.eye(3)
+
+    for Ph in range(0, numph):  # loop through rotation angles
+        Pr = Ph * np.pi / numph  # phase shift to be applied
+
+        # Apply phase shift to omega matrix:
+        z12 = om.copy() * np.exp(1j * Pr)
+        z12 = 0.5 * (z12 + np.transpose(np.conj(z12)))
+
+        # Solve the eigenvalue problem:
+        nu, w = linalg.eig(np.dot(linalg.inv(tm), z12))
+
+        wH = np.transpose(np.conj(w))
+
+        Tmp = np.dot(om, w)
+        Tmp12 = np.dot(wH, Tmp)
+
+        Tmp = np.dot(tm, w)
+        Tmp11 = np.dot(wH, Tmp)
+
+        l = np.argmin(nu)
+        gmin = Tmp12[l, l] / np.abs(Tmp11[l, l])  # min eigenvalue coherence
+
+        l = np.argmax(nu)
+        gmax = Tmp12[l, l] / np.abs(Tmp11[l, l])  # max eigenvalue coherence
+
+        gammaregion[Ph] = gmin
+        gammaregion[Ph + numph] = gmax
+
+        if (np.abs(gmax - gmin) > cohdiff):
+            cohdiff = np.abs(gmax - gmin)
+            gammamax = gmax
+            gammamin = gmin
+
+    gammaregion[-1] = gammaregion[
+        0]  # copy the first coherence to the end of the array, for a continuous coherence region plot
+
+    return gammamax, gammamin, gammaregion
--- Forest/make_pauli.py
+++ Forest/make_pauli.py
@@ -1,27 +1,35 @@
+import os
 import numpy as np
 import h5py
 import math
 
-def make_pauli(array_HH, array_VV, array_HV, row, col, filename_k1="pauli_k1.h5", filename_k1_T="pauli_k1_T.h5"):
-    # 计算 k11, k22, k33
+
+def make_pauli(array_HH, array_VV, array_HV, filename_k, filename_k_T, row, col, name_k, name_k_T):
     sqrt2 = math.sqrt(2)
     k11 = (array_HH + array_VV) / sqrt2
     k22 = (array_HH - array_VV) / sqrt2
     k33 = (array_HV * 2) / sqrt2
 
-    # 构建 k1 和 k1_T（也就是共轭转置）
     k1 = np.stack((k11, k22, k33), axis=-1)  # shape: (row, col, 3)
     k1 = k1[..., np.newaxis]  # shape: (row, col, 3, 1)
     k1_T = np.conj(k1).swapaxes(-1, -2)  # shape: (row, col, 1, 3)
 
+    # 获取目录路径
+    dir_k = os.path.dirname(filename_k)
+    dir_k_T = os.path.dirname(filename_k_T)
+
+    # 确保目录存在
+    os.makedirs(dir_k, exist_ok=True)
+    os.makedirs(dir_k_T, exist_ok=True)
+
     # 保存 k1
-    with h5py.File(filename_k1, 'w') as f:
-        f.create_dataset('k1', data=k1, dtype=k1.dtype)
-    print(f"------- Pauli 分解结束，k1 结果已保存至 {filename_k1} ----------")
+    with h5py.File(filename_k, 'w') as f:
+        f.create_dataset(name_k, (row,col,3,1),data=k1,dtype=k1.dtype)
+    print(f"✅ Pauli 分解结束，k1 结果已保存至 {filename_k}")
 
     # 保存 k1_T
-    with h5py.File(filename_k1_T, 'w') as f:
-        f.create_dataset('k1_T', data=k1_T, dtype=k1_T.dtype)
-    print(f"------- Pauli 分解结束，k1_T 结果已保存至 {filename_k1_T} ----------")
+    with h5py.File(filename_k_T, 'w') as f:
+        f.create_dataset(name_k_T, (row,col,1,3),data=k1_T, dtype=k1_T.dtype)
+    print(f"✅ Pauli 分解结束，k1_T 结果已保存至 {filename_k_T}")
 
     return k1, k1_T
